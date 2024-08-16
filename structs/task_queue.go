78cc677ed4020608a643dbdc8837f15318ed608a
MakayaYoel <yoelmakaya@gmail.com>
2024-08-15 23:15:55 -0400

    get rid of "tasks" slice

--- structs/task_queue.go
+++ structs/task_queue.go
@@ -3,7 +3,6 @@ package structs
 import "sync"
 
 type taskQueue struct {
-	tasks     []Task
 	enqueue   chan Task
 	dequeue   chan Task
 	waitGroup sync.WaitGroup
@@ -12,27 +11,18 @@ type taskQueue struct {
 // NewTaskQueue creates a new taskQueue struct.
 func NewTaskQueue() *taskQueue {
 	return &taskQueue{
-		tasks:   make([]Task, 0),
 		enqueue: make(chan Task),
 		dequeue: make(chan Task),
 	}
 }
 
-// Start continuously appends "tasks" from the enqueue channel into the tasks slice, or sends tasks from the "tasks" slice into the dequeue channel.
+// Start continuously sends tasks from the enqueue channel into the dequeue channel
 func (tQ *taskQueue) Start() {
 	go func() {
-		for {
-			select {
-			// Send tasks from enqueue channel into the "tasks" slice
-			case task := <-tQ.enqueue:
-				tQ.tasks = append(tQ.tasks, task)
-			default:
-				// Send tasks from the "tasks" slice into the dequeue channel
-				if len(tQ.tasks) > 0 {
-					tQ.dequeue <- tQ.tasks[0]
-					tQ.tasks = tQ.tasks[1:]
-				}
-			}
+		// Send tasks from enqueue channel into the dequeue channel
+		for task := range tQ.enqueue {
+			// Some features for verifying the task can wrap around the following
+			tQ.dequeue <- task
 		}
 	}()
 }
